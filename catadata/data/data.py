7d61a10801e648e83872b59dd54c2868b5cccc7b
myg0t <mjtaylorroberts@gmail.com>
2022-12-06 16:02:02 -0500

    print path

--- catadata/data/data.py
+++ catadata/data/data.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import geopandas as gpd
 import pathlib
-import sys
+import os
 
-print(pathlib.Path(".").resolve())
-sys.stdout.flush()
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print(dir_path)
 
-cata_data_path = pathlib.Path("./catadata/data/cata_data.csv")
-cata_parcels_path = pathlib.Path("./catadata/data/100m Riparian Zone.shp")
+cata_data_path = pathlib.Path(f"{dir_path}/cata_data.csv")
+cata_parcels_path = pathlib.Path(f"{dir_path}/100m Riparian Zone.shp")
 
 cata_data = pd.read_csv(cata_data_path.resolve(), dtype={"fid": int, "ALTPARNO": str})
 cata_parcels = gpd.read_file(cata_parcels_path.resolve(), dtype={"fid": str, "ALTPARNO": str})
